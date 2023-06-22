71d025282550a75eaf243bc7ae29553b3b56ebac
Wouter Zomerdijk <w.zomerdijk@tudelft.nl>
2023-06-22 12:15:38 +0200

    add test for subtract

--- test_example.py
+++ test_example.py
@@ -1,6 +1,12 @@
 from example import add
+from example import subtract
 
 
 def test_add():
     assert add(1, 2) == 3
     assert add(2, 8) == 10
+
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+    assert subtract(8, 2) == 6
